82ce9101b7443d97cfdf4aa96e9d6576c0781c8f
Greta Liu <gretaliu@Gretas-MacBook-Pro.local>
2026-03-27 17:53:14 -0400

    use env variables

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://uzoigrgwacmzufxbxocx.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6b2lncmd3YWNtenVmeGJ4b2N4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzODMzMTUsImV4cCI6MjA4OTk1OTMxNX0.eI13rWLrABXCbizv_ZUsrhas4-mJbXEe10olCQYBxas'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
